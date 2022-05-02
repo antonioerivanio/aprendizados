f573e21d99336836c78bfc21c5a1c6cff139f5ea
antonioerivanio <antonioerivanio@gmail.com>
2022-05-02 19:36:46 -0300

    primeiro commit

---
+++ src/main/java/com/aula10/ms/hrworks/HrWorksApplication.java
@@ -0,0 +1,13 @@
+package com.aula10.ms.hrworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorksApplication.class, args);
+	}
+
+}
