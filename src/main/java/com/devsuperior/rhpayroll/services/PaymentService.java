e43fe1178ff5fc04170eca0919225b9437881572
antonioerivanio <antonioerivanio@gmail.com>
2022-05-08 15:25:34 -0300

    feing

--- src/main/java/com/devsuperior/rhpayroll/services/PaymentService.java
+++ src/main/java/com/devsuperior/rhpayroll/services/PaymentService.java
@@ -10,21 +10,27 @@ import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.rhpayroll.entities.Payment;
 import com.devsuperior.rhpayroll.entities.Worker;
+import com.devsuperior.rhpayroll.services.feingClients.WorkersFeingClient;
 
 @Service
 public class PaymentService {
     
-    @Value("${hr-worker-host-url}")
-    private String workerHostUrl;
+    //@Value("${hr-worker-host-url}")
+    //private String workerHostUrl;
+    
+   // @Autowired
+    //private RestTemplate restTemplate;
     
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkersFeingClient  workersFeingClient;
     
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uri = new HashMap<>();
-        uri.put("id", "" + workerId);
-        
-        Worker worker = restTemplate.getForObject(workerHostUrl+ "/workers/{id}", Worker.class, uri);
+        /*
+         * Map<String, String> uri = new HashMap<>(); uri.put("id", "" + workerId);
+         * 
+         * Worker worker = restTemplate.getForObject(workerHostUrl+ "/workers/{id}", Worker.class, uri);
+         */
+        Worker worker = workersFeingClient.findById(workerId).getBody();
         System.out.println(worker);
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
