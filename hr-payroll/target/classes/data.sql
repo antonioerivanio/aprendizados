1dd7be577227b6b8af9f9407a5d546dbcea028fe
antonioerivanio <antonioerivanio@gmail.com>
2022-05-08 17:24:09 -0300

    adicionado

---
+++ hr-payroll/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into tb_work(name, DAILY_INCOME) values('Bob', 200.0);
+insert into tb_work(name, DAILY_INCOME) values('Maria', 300.0);
+insert into tb_work(name, DAILY_INCOME) values('Alex', 250.0);
