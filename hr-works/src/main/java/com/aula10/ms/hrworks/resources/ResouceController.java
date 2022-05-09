9cfb3680b1b5828a223e946905ab4ffd03563050
antonioerivanio <antonioerivanio@gmail.com>
2022-05-09 19:29:02 -0300

    adicionado a lib Ribbon de balecamento de carga

--- hr-works/src/main/java/com/aula10/ms/hrworks/resources/ResouceController.java
+++ hr-works/src/main/java/com/aula10/ms/hrworks/resources/ResouceController.java
@@ -4,7 +4,10 @@ package com.aula10.ms.hrworks.resources;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +22,17 @@ import com.aula10.ms.hrworks.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class ResouceController {  
     
+    private Logger logger = LoggerFactory.getLogger(ResouceController.class);
+    
+    @Autowired
+    private Environment environment;
+    
     @Autowired
     WorkerRepository repositoryWork;
     
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
+        
         Iterable<Worker> resultIt = repositoryWork.findAll();
                 
         return ResponseEntity.ok((List)resultIt);
@@ -31,6 +40,8 @@ public class ResouceController {
     
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("PORT: " +  environment.getProperty("local.server.port"));
+        
         Optional<Worker> bean = repositoryWork.findById(id);
                 
         return ResponseEntity.ok(bean.get());
