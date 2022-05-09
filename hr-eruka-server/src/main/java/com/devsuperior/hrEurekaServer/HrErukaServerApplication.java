d957691aa7ebe048a0b82ededadfe9d5244f5255
antonioerivanio <antonioerivanio@gmail.com>
2022-05-09 19:50:01 -0300

    criado projeto eruka server

---
+++ hr-eruka-server/src/main/java/com/devsuperior/hrEurekaServer/HrErukaServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrErukaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrErukaServerApplication.class, args);
+	}
+
+}
